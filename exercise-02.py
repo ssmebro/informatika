8ba14e84a5816a1782928d5a92bdf5c355166bb1
ssmebro <kiryusha.sinin@gmail.com>
2021-11-13 18:28:04 +0300

    Create exercise-02.py

---
+++ exercise-02.py
@@ -0,0 +1,7 @@
+def add1v1(x : int) -> int:
+  return x + 1
+  
+def add1v2(x : int) -> int:
+  x + 1
+ 
+'''Функция add1v1 возвращает значение при помощи return, а функция add1v2 не возвращает, потому что отсутствует return'''
